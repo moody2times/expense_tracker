30e0661be8cc4861a04c1e35242bf5b2591e7e6a
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-08-29 19:36:22 +0100

    :art: Update the code

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ const App = () => {
 	return (
 		<div className="App">
 			<h1 className="heading">Expense tracker</h1>
-			<ExpenseItem date="2 days ago" description="Guitar" amount="$150" />
-			<ExpenseItem description="JavaScript Course" amount="$50" />
-			<ExpenseItem description="Smart Phone" amount="$300" />
+			<ExpenseItem date="85-26-12" description="Guitar" amount="150" />
+			<ExpenseItem
+				date="17/06/95"
+				description="JavaScript Course"
+				amount="50"
+			/>
+			<ExpenseItem
+				date="17/04/2021"
+				description="Smart Phone"
+				amount="300"
+			/>
 		</div>
 	);
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,31 @@
+// import { useState } from "react";
 import "./Expense.css";
 
 const ExpenseItem = (props) => {
+	// const [amount, setAmount] = useState(props.amount);
+	const locale = navigator.locale;
+
 	const timeStamp = new Date();
+	const month = timeStamp.toLocaleString(locale, { month: "long" });
+	const year = timeStamp.toLocaleString(locale, { year: "numeric" });
+	const date = timeStamp.toLocaleString(locale, { day: "numeric" });
+
+	// const handleClick = () => {
+	// 	setAmount(+amount + 1);
+	// };
 
 	return (
 		<ul className="list__container">
-			<li>{timeStamp.toLocaleDateString()}</li>
-			<li>{props.description}</li>
-			<li>{props.amount}</li>
+			<li className="list__date">
+				<span className="list__date--month">{month}</span>
+				<span className="list__date--date">{date}</span>
+				<span className="list__date--year">{year}</span>
+			</li>
+			<li className="list__description">{props.description}</li>
+			<li className="list__amount">${props.amount}</li>
+			{/* <button className="btn" onClick={handleClick}>
+				Add Expense
+			</button> */}
 		</ul>
 	);
 };
