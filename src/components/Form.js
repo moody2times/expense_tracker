a5a1f6398d20ed80d5cfdba608f64ca8765c9de0
Mudiaga Uwojeya <moody.mudiaga@outlook.com>
2022-02-09 20:08:22 +0100

    :construction: Update uniqIdGenerator import

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import { useRef, useContext, useReducer, useEffect } from "react";
 import "../styles/Form.css";
 import { Button } from "./Button";
 import inputRef from "../Context/inputContext";
-import { uniqIdGenerator } from "../helper/randomId";
+import { uniqIdGenerator } from "../helper/uniqIdGenerator";
 
 //initial formData state
 const formState = {
