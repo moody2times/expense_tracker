f7404db41178af1ab48d3f623c0bbba193abd04a
Mudiaga Uwojeya <moody.mudiaga@outlook.com>
2022-02-09 19:58:17 +0100

    :fire: Remove random id generator code from Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -82,15 +82,6 @@ const Form = (props) => {
 		if (title.trim() === "" || amount.trim() === "" || date.trim() === "")
 			return;
 
-		//generate random id
-		const uniqIdGenerator = () => {
-			const uniqId = () =>
-				Math.floor((Math.random() + 1) * 0x10000)
-					.toString(16)
-					.substring(1);
-			return `${uniqId()}-${uniqId()}-${uniqId()}`;
-		};
-
 		//expense object
 		const expenseData = {
 			title,
